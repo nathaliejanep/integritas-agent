5ce3680eff4fc2e9737641b93f7b380c557af7e3
devmus <web3devmus@gmail.com>
2025-08-21 16:07:43 +0200

    asdf

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,6 +6,7 @@ module.exports = {
       cwd: "/home/integritas-agent",
       script: "/home/integritas-agent/venv/bin/python", // run python itself
       args: "-m app.agent", // pass -m app.agent
+      time: true,
     },
   ],
 };
