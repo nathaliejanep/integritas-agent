add7611f72e187d501e717e78b00d28b4f550717
devmus <web3devmus@gmail.com>
2025-08-21 15:58:04 +0200

    update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,15 +1,18 @@
-// ecosystem.config.js
+// // ecosystem.config.js
 module.exports = {
   apps: [
     {
       name: "agent",
-      script: "asi_integritas_agent.py", // your Python script
-      interpreter: "venv/bin/python", // use the Python from your virtualenv
-      time: true, // show timestamps in logs
-      autorestart: true, // restart on crash
-      min_uptime: "5s", // process considered up after 5s
-      restart_delay: 5000, // wait 5s before restarting
-      max_restarts: 10, // avoid infinite restart loops
+      cwd: "/home/integritas-agent",
+      script: "app.agent",
+      interpreter: "/home/integritas-agent/venv/bin/python",
+      interpreter_args: "-m",
+      exec_mode: "fork",
+      time: true,
+      autorestart: true,
+      min_uptime: "5s",
+      restart_delay: 5000,
+      max_restarts: 10,
     },
   ],
 };
